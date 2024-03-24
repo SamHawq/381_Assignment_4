ae4e2328a51b69bc6e6868bfa8cc5230091d0f09
Samiul Haque <mdsamiul.haque@ucalgary.ca>
2024-03-23 19:02:37 -0600

    progress

---
+++ my-ecommerce-app/src/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+const imagePath = '/images/logo.png';
+
+function Home() {
+    return (
+        <div className="my_ecommerce_app">
+        <header className="App-header">
+            <img src={imagePath} className="App-logo" alt="logo" />
+            <h3 className="header-txt">
+            Company Name
+            </h3>
+        </header>
+
+        <section className="navigation">
+            <a href='/'> Home </a>
+            <a href='/Products'> Products </a>
+            <a href='/Login'> Login </a>
+        </section>
+
+        <section className='About-us'>
+            <h2>About Us</h2>
+            <p>Welcome to out online store! We are passionate about provided high-quality products 
+                and exceptional customer service. Learn more about our story and commitment to your 
+                satisfaction</p>
+        </section>
+        </div>
+    );
+}
+
+export default Home;
+
