3414b09523eed222c94ebc89b5afcf7c0f9541ef
manbirbriar <manbir.briar@ucalgary.ca>
2024-03-25 21:50:25 -0600

    $

--- my-ecommerce-app/src/components/CartItem.js
+++ my-ecommerce-app/src/components/CartItem.js
@@ -5,9 +5,9 @@ const CartItem = ({ item, removeFromCart }) => {
     <div className="cart-item">
       <img src={'/'+item.image} alt={item.name}  style={{ maxWidth: '200px', height: 'auto' }}/>
       <p>{item.name}</p>
-      <p>{item.price}</p>
+      <p>{'$'+item.price}</p>
       <p>Quantity: {item.quantity}</p>
-      <p>Total: {item.price * item.quantity}</p>
+      <p>Total: {'$'+item.price * item.quantity}</p>
       <button onClick={() => removeFromCart(item)}>Remove</button>
     </div>
   );
